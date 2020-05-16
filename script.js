e83b114281b037172543099644cfffa313ba9a51
Eduardo Guillen <eguillen117@gmail.com>
2020-05-16 08:10:04 -0500

    Clean up push
    Minor clean up

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-// /* /*  */do I need a separate javascript or should I embed into the index?// */
-//< !--End of Script Sources JQUERY and Moment.js ================================================
-// Start of Function ===================================
-
-//getting the localStorage key values
+//getting the localStorage key values=================
 if (localStorage.getItem('calendarData') !== null) {
 	calendarData = JSON.parse(localStorage.getItem('calendarData'));
 }
@@ -20,10 +16,61 @@ $(document).ready(function() {
 
 	//this should be the empty area to allow the user to add text.
 	var userInput = '';
-	// start of function... should this be in a sep file or embedded all in index.html?
+
 	//moment.js code should get current date....
 	$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
 	//userText created for user content.
 	let userText = $('#userContent');
+
+	//creating for each object(entry) of the array calendarData appending the rows and th,td, buttons
+	calendarData.forEach((entry) => {
+		//current hour
+		var currentHour = moment().hour();
+
+		var styleClass = '';
+
+		// Develop the functionality to handle the color in each timeblock
+		if (currentHour === entry.id) {
+			styleClass = 'present';
+		} else if (currentHour < entry.id) {
+			styleClass = 'future';
+		} else if (currentHour > entry.id) {
+			styleClass = 'past';
+		}
+
+		tbody.append(`
+        <tr class="rowClass"">
+			<th style="background-color: whitesmoke; border-right: 3px solid teal; vertical-align: middle;">
+			 ${entry.hour}
+			</th>
+        	<td class="pl-0 ${styleClass}" style="width: 780px;padding-top: 0px;padding-bottom: 0px;padding-right: 0px;">
+				<textarea id="${entry.id}" style="width: 100%; padding-top: 0px;padding-bottom: 0px; padding-left: 0px; padding-right: 0px; height: 80px; border-left: 0px;" class="textareaClass description">${entry.event}</textarea>
+			</td>
+			<td class="pt-0 pb-0 pl-0">
+				<button data-ref="${entry.id}" class="saveBtn btn btn-lg bg-info text-white btn-outline-info waves-effect" style= "height: 80px; width: 100px;"><i class="fas fa-thumbtack" aria-hidden="true"></i></button>
+			</td>
+		</tr>`);
+	});
+
+	// save button event
+	$('.saveButton').on('click', saveEvent);
+
+	/**
+	 * save event target the specific id from the button
+	 * and using this id to get the textarea associatte with it
+	 */
+	function saveEvent() {
+		textareaId = $(this).attr('data-ref');
+		var valueAttr = $('#' + textareaId).val();
+
+		calendarData.forEach((timeblok) => {
+			if (timeblok.id == textareaId) {
+				timeblok.event = valueAttr;
+			}
+		});
+
+		//save the set value(s) to the localStorage using JSON.stringify("object...")
+		localStorage.setItem('calendarData', JSON.stringify(calendarData));
+	}
 });
