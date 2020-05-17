9fe3e4dea48d6ba5c5f51ea1d14b1d0fa406653c
Eduardo Guillen <eguillen117@gmail.com>
2020-05-16 22:42:09 -0500

    attempting to show Text area boxes

--- script.js
+++ script.js
@@ -1,70 +1,24 @@
-var calendarData = [
-	{
-		hour: '8AM',
-		event: '',
-		id: 9
-	},
-	{
-		hour: '9AM',
-		event: '',
-		id: 10
-	},
-	{
-		hour: '10AM',
-		event: '',
-		id: 11
-	},
-	{
-		hour: '11AM',
-		event: '',
-		id: 12
-	},
-	{
-		hour: '12PM',
-		event: '',
-		id: 13
-	},
-	{
-		hour: '1PM',
-		event: '',
-		id: 14
-	},
-	{
-		hour: '2PM',
-		event: '',
-		id: 15
-	},
-	{
-		hour: '3PM',
-		event: '',
-		id: 16
-	},
-	{
-		hour: '4PM',
-		event: '',
-		id: 17
-	}
-];
+var calendarData = time - block;
 
 //getting the localStorage key values=================
 if (localStorage.getItem('calendarData') !== null) {
 	calendarData = JSON.parse(localStorage.getItem('calendarData'));
 }
 
+// // Console.logs to test =============================================
+// console.log('Function is ready');
+// console.log(moment().format('dddd') + moment().format('MMM Do'));
+// console.log(moment().calendar());
+// console.log(moment().format());
+// // ===================================================================
+// // Testing to see if the moment is console logging correctly
+// const m = moment();
+// console.log(m.toString());
+
+// //this should be the empty area to allow the user to add text.
+// var userInput = '';
+
 $(document).ready(function() {
-	// // Console.logs to test =============================================
-	// console.log('Function is ready');
-	// console.log(moment().format('dddd') + moment().format('MMM Do'));
-	// console.log(moment().calendar());
-	// console.log(moment().format());
-	// // ===================================================================
-	// // Testing to see if the moment is console logging correctly
-	// const m = moment();
-	// console.log(m.toString());
-
-	// //this should be the empty area to allow the user to add text.
-	// var userInput = '';
-
 	$('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 	var tbody = $('#calendar-content');
 	calendarData.forEach((entry) => {
